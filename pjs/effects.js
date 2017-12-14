4a6aae499a144316d49108bdeff1fd494ebee10a
Branden Ng <brandendanielng@gmail.com>
2017-12-14 16:15:30 +1100

    style changes

--- pjs/effects.js
+++ pjs/effects.js
@@ -1,15 +1,15 @@
 // MOBILE
 $('.mobile i').hover(function() {
   $('.mobile h3 a').css('text-decoration', 'underline');
-  $('.mobile i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $(this).css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
 }, function() {
   $('.mobile h3 a').css('text-decoration', 'none');
-  $('.mobile i').css('background', 'rgba(255,255,255,.25)');
-  $('.mobile i').css('color', '#333');
+  $(this).css('background', 'rgba(255,255,255,.25)');
+  $(this).css('color', '#333');
 });
 
 $('.mobile h3 a').hover(function() {
-  $('.mobile i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.mobile i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.mobile i').css({'background': 'rgba(255,255,255,.25)', 'transition': '.4s', 'color': '#333'});
@@ -19,7 +19,7 @@ $('.mobile h3 a').hover(function() {
 // EMAIL
 $('.email i').hover(function() {
   $('.email h3 a').css('text-decoration', 'underline');
-  $('.email i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.email i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
 }, function() {
   $('.email h3 a').css('text-decoration', 'none');
   $('.email i').css('background', 'rgba(255,255,255,.25)');
@@ -27,7 +27,7 @@ $('.email i').hover(function() {
 });
 
 $('.email h3 a').hover(function() {
-  $('.email i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.email i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.email i').css({'background': 'rgba(255,255,255,.25)', 'transition': '.4s', 'color': '#333'});
@@ -37,7 +37,7 @@ $('.email h3 a').hover(function() {
 // GITHUB
 $('.github i').hover(function() {
   $('.github h3 a').css('text-decoration', 'underline');
-  $('.github i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.github i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
 }, function() {
   $('.github h3 a').css('text-decoration', 'none');
   $('.github i').css('background', 'rgba(255,255,255,.25)');
@@ -45,7 +45,7 @@ $('.github i').hover(function() {
 });
 
 $('.github h3 a').hover(function() {
-  $('.github i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.github i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.github i').css({'background': 'rgba(255,255,255,.25)', 'transition': '.4s', 'color': '#333'});
@@ -55,7 +55,7 @@ $('.github h3 a').hover(function() {
 // LINKEDIN
 $('.linkedin i').hover(function() {
   $('.linkedin h3 a').css('text-decoration', 'underline');
-  $('.linkedin i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.linkedin i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
 }, function() {
   $('.linkedin h3 a').css('text-decoration', 'none');
   $('.linkedin i').css('background', 'rgba(255,255,255,.25)');
@@ -63,7 +63,7 @@ $('.linkedin i').hover(function() {
 });
 
 $('.linkedin h3 a').hover(function() {
-  $('.linkedin i').css({'background': 'rgba(0,0,0,.2)', 'color': '#bbb'});
+  $('.linkedin i').css({'background': 'rgba(0,0,0,.1)', 'color': '#333'});
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.linkedin i').css({'background': 'rgba(255,255,255,.25)', 'transition': '.4s', 'color': '#333'});
