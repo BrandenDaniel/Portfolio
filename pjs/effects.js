6e3c09e2b9536b42a331f3edfc80557768ae45a6
Branden Ng <brandendanielng@gmail.com>
2017-12-27 16:51:03 +1100

    style changes

--- pjs/effects.js
+++ pjs/effects.js
@@ -1,71 +1,71 @@
 // MOBILE
 $('.mobile i').hover(function() {
   $('.mobile h3 a').css('text-decoration', 'underline');
-  $(this).css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $(this).css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
 }, function() {
   $('.mobile h3 a').css('text-decoration', 'none');
-  $(this).css('background', 'rgba(255,255,255, 0)');
-  $(this).css('color', '#333');
+  $(this).css('background', 'rgba(0,0,0, 0)');
+  $(this).css('color', '#829693');
 });
 
 $('.mobile h3 a').hover(function() {
-  $('.mobile i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.mobile i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.mobile i').css({'background': 'rgba(255,255,255,0)', 'transition': '.4s', 'color': '#333'});
+  $('.mobile i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
   $(this).css('text-decoration', 'none');
 });
 
 // EMAIL
 $('.email i').hover(function() {
   $('.email h3 a').css('text-decoration', 'underline');
-  $('.email i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.email i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
 }, function() {
   $('.email h3 a').css('text-decoration', 'none');
-  $('.email i').css('background', 'rgba(255,255,255,0)');
-  $('.email i').css('color', '#333');
+  $('.email i').css('background', 'rgba(0,0,0,0)');
+  $('.email i').css('color', '#829693');
 });
 
 $('.email h3 a').hover(function() {
-  $('.email i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.email i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.email i').css({'background': 'rgba(255,255,255,0)', 'transition': '.4s', 'color': '#333'});
+  $('.email i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
   $(this).css('text-decoration', 'none');
 });
 
 // GITHUB
 $('.github i').hover(function() {
   $('.github h3 a').css('text-decoration', 'underline');
-  $('.github i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.github i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
 }, function() {
   $('.github h3 a').css('text-decoration', 'none');
-  $('.github i').css('background', 'rgba(255,255,255,0)');
-  $('.github i').css('color', '#333');
+  $('.github i').css('background', 'rgba(0,0,0,0)');
+  $('.github i').css('color', '#829693');
 });
 
 $('.github h3 a').hover(function() {
-  $('.github i').css({'background': 'rgba(255,255,255,0.76)', 'color': '#333'});
+  $('.github i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.github i').css({'background': 'rgba(255,255,255,0)', 'transition': '.4s', 'color': '#333'});
+  $('.github i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
   $(this).css('text-decoration', 'none');
 });
 
 // LINKEDIN
 $('.linkedin i').hover(function() {
   $('.linkedin h3 a').css('text-decoration', 'underline');
-  $('.linkedin i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.linkedin i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
 }, function() {
   $('.linkedin h3 a').css('text-decoration', 'none');
-  $('.linkedin i').css('background', 'rgba(255,255,255,0)');
-  $('.linkedin i').css('color', '#333');
+  $('.linkedin i').css('background', 'rgba(0,0,0,0)');
+  $('.linkedin i').css('color', '#829693');
 });
 
 $('.linkedin h3 a').hover(function() {
-  $('.linkedin i').css({'background': 'rgba(255,255,255,0.65)', 'color': '#333'});
+  $('.linkedin i').css({'background': 'rgba(0,0,0,00.2)', 'color': '#829693'});
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.linkedin i').css({'background': 'rgba(255,255,255,0)', 'transition': '.4s', 'color': '#333'});
+  $('.linkedin i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
   $(this).css('text-decoration', 'none');
 });
