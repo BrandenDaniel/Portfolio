253d1b7670a4a04840cdf4f63de83dda4b47f057
Branden Ng <brandendanielng@gmail.com>
2017-12-30 00:59:49 +1100

    style changes

--- pjs/effects.js
+++ pjs/effects.js
@@ -1,7 +1,10 @@
 // MOBILE
 $('.mobile i').hover(function() {
   $('.mobile h3 a').css('text-decoration', 'underline');
-  $(this).css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $(this).css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
 }, function() {
   $('.mobile h3 a').css('text-decoration', 'none');
   $(this).css('background', 'rgba(0,0,0, 0)');
@@ -9,17 +12,27 @@ $('.mobile i').hover(function() {
 });
 
 $('.mobile h3 a').hover(function() {
-  $('.mobile i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.mobile i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.mobile i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
+  $('.mobile i').css({
+    'background': 'rgba(0,0,0,0)',
+    'transition': '.4s',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'none');
 });
 
 // EMAIL
 $('.email i').hover(function() {
   $('.email h3 a').css('text-decoration', 'underline');
-  $('.email i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.email i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
 }, function() {
   $('.email h3 a').css('text-decoration', 'none');
   $('.email i').css('background', 'rgba(0,0,0,0)');
@@ -27,17 +40,27 @@ $('.email i').hover(function() {
 });
 
 $('.email h3 a').hover(function() {
-  $('.email i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.email i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.email i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
+  $('.email i').css({
+    'background': 'rgba(0,0,0,0)',
+    'transition': '.4s',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'none');
 });
 
 // GITHUB
 $('.github i').hover(function() {
   $('.github h3 a').css('text-decoration', 'underline');
-  $('.github i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.github i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
 }, function() {
   $('.github h3 a').css('text-decoration', 'none');
   $('.github i').css('background', 'rgba(0,0,0,0)');
@@ -45,17 +68,27 @@ $('.github i').hover(function() {
 });
 
 $('.github h3 a').hover(function() {
-  $('.github i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.github i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.github i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
+  $('.github i').css({
+    'background': 'rgba(0,0,0,0)',
+    'transition': '.4s',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'none');
 });
 
 // LINKEDIN
 $('.linkedin i').hover(function() {
   $('.linkedin h3 a').css('text-decoration', 'underline');
-  $('.linkedin i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.linkedin i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
 }, function() {
   $('.linkedin h3 a').css('text-decoration', 'none');
   $('.linkedin i').css('background', 'rgba(0,0,0,0)');
@@ -63,9 +96,16 @@ $('.linkedin i').hover(function() {
 });
 
 $('.linkedin h3 a').hover(function() {
-  $('.linkedin i').css({'background': 'rgba(0,0,0,0.2)', 'color': '#829693'});
+  $('.linkedin i').css({
+    'background': 'rgba(0,0,0,0.2)',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'underline');
 }, function() {
-  $('.linkedin i').css({'background': 'rgba(0,0,0,0)', 'transition': '.4s', 'color': '#829693'});
+  $('.linkedin i').css({
+    'background': 'rgba(0,0,0,0)',
+    'transition': '.4s',
+    'color': '#829693'
+  });
   $(this).css('text-decoration', 'none');
 });
