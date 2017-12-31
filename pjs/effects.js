81f2bc79b1677d37c94b938c024252afd9c1f722
Branden Ng <brandendanielng@gmail.com>
2018-01-01 03:11:49 +1100

    style changes

--- pjs/effects.js
+++ pjs/effects.js
@@ -3,25 +3,25 @@ $('.mobile i').hover(function() {
   $('.mobile h3 a').css('text-decoration', 'underline');
   $(this).css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
 }, function() {
   $('.mobile h3 a').css('text-decoration', 'none');
   $(this).css('background', 'rgba(0,0,0, 0)');
-  $(this).css('color', '#829693');
+  $(this).css('color', '#9ca3a3');
 });
 
 $('.mobile h3 a').hover(function() {
   $('.mobile i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.mobile i').css({
     'background': 'rgba(0,0,0,0)',
     'transition': '.4s',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'none');
 });
@@ -31,25 +31,25 @@ $('.email i').hover(function() {
   $('.email h3 a').css('text-decoration', 'underline');
   $('.email i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
 }, function() {
   $('.email h3 a').css('text-decoration', 'none');
   $('.email i').css('background', 'rgba(0,0,0,0)');
-  $('.email i').css('color', '#829693');
+  $('.email i').css('color', '#9ca3a3');
 });
 
 $('.email h3 a').hover(function() {
   $('.email i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.email i').css({
     'background': 'rgba(0,0,0,0)',
     'transition': '.4s',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'none');
 });
@@ -59,25 +59,25 @@ $('.github i').hover(function() {
   $('.github h3 a').css('text-decoration', 'underline');
   $('.github i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
 }, function() {
   $('.github h3 a').css('text-decoration', 'none');
   $('.github i').css('background', 'rgba(0,0,0,0)');
-  $('.github i').css('color', '#829693');
+  $('.github i').css('color', '#9ca3a3');
 });
 
 $('.github h3 a').hover(function() {
   $('.github i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.github i').css({
     'background': 'rgba(0,0,0,0)',
     'transition': '.4s',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'none');
 });
@@ -87,25 +87,25 @@ $('.linkedin i').hover(function() {
   $('.linkedin h3 a').css('text-decoration', 'underline');
   $('.linkedin i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
 }, function() {
   $('.linkedin h3 a').css('text-decoration', 'none');
   $('.linkedin i').css('background', 'rgba(0,0,0,0)');
-  $('.linkedin i').css('color', '#829693');
+  $('.linkedin i').css('color', '#9ca3a3');
 });
 
 $('.linkedin h3 a').hover(function() {
   $('.linkedin i').css({
     'background': 'rgba(0,0,0,0.2)',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'underline');
 }, function() {
   $('.linkedin i').css({
     'background': 'rgba(0,0,0,0)',
     'transition': '.4s',
-    'color': '#829693'
+    'color': '#9ca3a3'
   });
   $(this).css('text-decoration', 'none');
 });
