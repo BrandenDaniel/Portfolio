07aed08d2ecbfaaa27f3c3101cf2cb2c54180ef1
Branden Ng <brandendanielng@gmail.com>
2018-01-18 23:55:21 +1100

    style changes

--- pjs/effects.js
+++ pjs/effects.js
@@ -111,17 +111,19 @@
 // });
 
 
-$('.hamburger-icon').click(function(){
-   $('.mobile-nav').css('display', 'block');
-   $('body').css('overflow', 'hidden');
+$('.hamburger-icon').click(function() {
+  $('.mobile-nav').css('display', 'block');
+  $('body').css('overflow', 'hidden');
 });
 
-$('.x').click(function(){
+$('.x').click(function() {
   $('.mobile-nav').css('display', 'none');
   $('body').css('overflow', 'auto');
 });
 
-$('.back-to-top').click(function(){
-		$('html, body').animate({scrollTop : 0},500);
-		return false;
-	});
+$('.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
