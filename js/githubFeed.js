ef8ec1f3003d242f84422a6c757f571ace7cd3d5
Branden Ng <brandendanielng@gmail.com>
2018-02-14 19:39:35 +1100

    style changes

--- js/githubFeed.js
+++ js/githubFeed.js
@@ -2,129 +2,124 @@ var repoData;
 var contribution = 12; //12 added contributions due to deleted projects that github persist to display
 var addition = null;
 
-var repoRequest = new XMLHttpRequest();
-repoRequest.open('GET', 'https://api.github.com/users/brandendaniel');
-repoRequest.onload = function() {
-  var data = JSON.parse(repoRequest.responseText);
-  repoData = data.public_repos;
-  $('.repoData').html(repoData);
-};
-// Send it
-repoRequest.send();
 
-var githubIoReq = new XMLHttpRequest();
-githubIoReq.open('GET', 'https://api.github.com/repos/brandendaniel/BrandenDaniel.github.io/stats/contributors');
-githubIoReq.onload = function() {
-  var commit = JSON.parse(githubIoReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/users/brandendaniel?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    repoData = data.public_repos;
+    $('.repoData').html(repoData);
   }
-  // curl -i 'https://api.github.com/users/brandendaniel?client_id=007edf113f22d4b15831&client_secret=eb161a20989f4812d71c8e3cab6c546f9057bb41'
+});
 
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/BrandenDaniel.github.io/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
 
-  // if (githubIoReq.status === )
-
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-githubIoReq.send();
-
-var folioReq = new XMLHttpRequest();
-folioReq.open('GET', 'https://api.github.com/repos/brandendaniel/Portfolio/stats/contributors');
-folioReq.onload = function() {
-  var commit = JSON.parse(folioReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
   }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-folioReq.send();
+});
 
-var froggerReq = new XMLHttpRequest();
-froggerReq.open('GET', 'https://api.github.com/repos/brandendaniel/FroggerSimulator/stats/contributors');
-froggerReq.onload = function() {
-  var commit = JSON.parse(froggerReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
-  }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-froggerReq.send();
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/Portfolio/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
 
-var matchingReq = new XMLHttpRequest();
-matchingReq.open('GET', 'https://api.github.com/repos/brandendaniel/MatchingGame/stats/contributors');
-matchingReq.onload = function() {
-  var commit = JSON.parse(matchingReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
   }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-matchingReq.send();
+});
 
-var eComReq = new XMLHttpRequest();
-eComReq.open('GET', 'https://api.github.com/repos/brandendaniel/E-commerce-Project/stats/contributors');
-eComReq.onload = function() {
-  var commit = JSON.parse(eComReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
-  }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-eComReq.send();
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/FroggerSimulator/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
 
-var pixelArtReq = new XMLHttpRequest();
-pixelArtReq.open('GET', 'https://api.github.com/repos/brandendaniel/Pixel-Art-Maker/stats/contributors');
-pixelArtReq.onload = function() {
-  var commit = JSON.parse(pixelArtReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
   }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-pixelArtReq.send();
+});
 
-var owQuoteReq = new XMLHttpRequest();
-owQuoteReq.open('GET', 'https://api.github.com/repos/brandendaniel/Overwatch-Quote-Generator/stats/contributors');
-owQuoteReq.onload = function() {
-  var commit = JSON.parse(owQuoteReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
-  }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-owQuoteReq.send();
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/MatchingGame/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
 
-var cmsReq = new XMLHttpRequest();
-cmsReq.open('GET', 'https://api.github.com/repos/brandendaniel/CMS-theme-BootStrap/stats/contributors');
-cmsReq.onload = function() {
-  var commit = JSON.parse(cmsReq.responseText);
-  contribution += commit[0].total;
-  for (var i = 0; i < commit[0].weeks.length; i++) {
-    addition += commit[0].weeks[i].a;
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
   }
-  $('.contribution').html(contribution);
-  $('.addition').html(addition);
-};
-// Send it
-cmsReq.send();
+});
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/E-commerce-Project/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
+
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
+  }
+});
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/Pixel-Art-Maker/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
+
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
+  }
+});
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/Overwatch-Quote-Generator/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
+
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
+  }
+});
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.github.com/repos/brandendaniel/CMS-theme-BootStrap/stats/contributors?access_token=4388b2658182341d61c1506bdbf249d49d5f2acc',
+  success: function(data) {
+    contribution += data[0].total;
+
+    for (var i=0; i < data[0].weeks.length; i++) {
+      addition += data[0].weeks[i].a;
+    }
+    $('.contribution').html(contribution);
+    $('.addition').html(addition);
+  }
+});
