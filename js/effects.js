3ab9ff34b18bfd6d938cd5119bd0b5942b6cb5d5
Branden Ng <brandendanielng@gmail.com>
2018-04-07 02:41:20 +1000

    style changes

--- js/effects.js
+++ js/effects.js
@@ -6,7 +6,7 @@ $('.back-to-top').click(function() {
 });
 
 $('#open-nav').click(function() {
-  $('.mobile-nav').css('width', '65%');
+  $('.mobile-nav').css('width', '60%');
   $('.hamburger-container').css('display', 'none');
   $('body').css('overflow', 'hidden');
 });
